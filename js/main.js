24adf977561d161f3aaecdd951ec0eddb047733b
cochy98 <cosmoferrigno@gmail.com>
2022-03-24 16:19:39 +0100

    aggiunta funzione 'generateRandomEmail'

--- js/main.js
+++ js/main.js
@@ -2,10 +2,22 @@ const app = new Vue(
     {
         el: '#app',
         data: {
-
+            randomEmail: []
         },
         methods: {
-            
+            generateRandomEmail(){
+                // Resetto l'array delle email random ad ogni click sul pulsante
+                this.randomEmail = [];
+
+                // Genero 10 email casuali e le inserisco nell'array
+                for(let i = 0; i < 10; i ++){
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (response) => {
+                        const result = response.data;
+                        console.log(result.response);
+                        this.randomEmail.push(result.response);
+                    });
+                }
+            }
         }
     }
 );
